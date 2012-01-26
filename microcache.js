1e76eb7ceacbbafeda38b03716e65652e7bb4ce6
Ryan Lynch <ryan.lynch@eia.gov>
2012-01-26 16:31:06 -0500

    Added AMD wrapper in addition to common js and global export

--- microcache.js
+++ microcache.js
@@ -5,25 +5,33 @@
  * 
  * @tags inmemory, keyvalue, cache, node, browser
 */
-var MicroCache	= function(){
-	var _values	= {};
-	return {
-		get	: function(key){ return _values[key];	},
-		contains: function(key){ return key in _values;	},
-		remove	: function(key){ delete _values[key];	},
-		set	: function(key, value){	_values[key] = value;},
-		values	: function(){ return _values;	},
-		getSet	: function(key, value){
-			if( !this.contains(key) ){
-				this.set(key, typeof value == 'function' ? value() : value )
+(function(define){
+	define('MicroCache', function(){
+		return function(){
+			var _values	= {};
+			return {
+				get	: function(key){ return _values[key];	},
+				contains: function(key){ return key in _values;	},
+				remove	: function(key){ delete _values[key];	},
+				set	: function(key, value){	_values[key] = value;},
+				values	: function(){ return _values;	},
+				getSet	: function(key, value){
+					if( !this.contains(key) ){
+						this.set(key, typeof value == 'function' ? value() : value )
+					}
+					return this.get(key);
+				}
 			}
-			return this.get(key);
+		}
+	})
+}(typeof define === 'function' && ('amd' in define) ? define :
+	function (id, factory) {
+		if (typeof module !== 'undefined' && ('exports' in module)) {
+			// export in common js
+			module.exports = factory();
+		} else {
+			// Export to global ns
+			window[id] = factory();
 		}
 	}
-}
-
-
-// export in common js
-if( typeof module !== "undefined" && ('exports' in module)){
-	module.exports	= MicroCache;
-}
+));
